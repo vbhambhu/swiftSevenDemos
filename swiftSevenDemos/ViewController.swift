f3284de911f440d2215d8ef9df19202e8718e5f6
vkumar <v.bhambhu083@gmail.com>
2018-03-27 15:55:54 +0100

    Initial Commit

---
+++ swiftSevenDemos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftSevenDemos
+//
+//  Created by Vinod Kumar on 27/03/2018.
+//  Copyright © 2018 Vinod Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
